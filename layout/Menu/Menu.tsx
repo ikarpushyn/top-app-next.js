e38779447b776688206ee536ce9dcfbee6ee06af
Ilija <68541501+ikarpushyn@users.noreply.github.com>
2023-01-18 21:40:59 +0200

    add search pages

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -3,36 +3,45 @@ import styles from './Menu.module.css';
 import cn from 'classnames';
 import { AppContext } from '../../context/app.context';
 import { FirstLevelMenuItem, PageItem } from '../../interfaces/menu.interface';
-import CoursesIcon from '../../helpers/icons/courses.svg';
-import ServicesIcon from '../../helpers/icons/services.svg';
-import BooksIcon from '../../helpers/icons/books.svg';
-import ProductsIcon from '../../helpers/icons/products.svg';
-import { TopLevelCategory } from '../../interfaces/page.interface';
-const firstLevelMenu: FirstLevelMenuItem[] = [
-	{ route: 'courses', name: 'Курсы', icon: <CoursesIcon />, id: TopLevelCategory.Courses },
-	{ route: 'services', name: 'Сервисы', icon: <ServicesIcon />, id: TopLevelCategory.Services },
-	{ route: 'books', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
-	{ route: 'products', name: 'Продукты', icon: <ProductsIcon />, id: TopLevelCategory.Products },
-];
+
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { firstLevelMenu } from '../../helpers/helpers';
 
 export const Menu = (): JSX.Element => {
 	const { menu, setMenu, firstCategory } = useContext(AppContext);
+	const router = useRouter();
+
+	const openSecondLevel = (secondCategory: string) => {
+		setMenu &&
+			setMenu(
+				menu.map((m) => {
+					if (m._id.secondCategory == secondCategory) {
+						m.isOpened = !m.isOpened;
+					}
+					return m;
+				})
+			);
+	};
 
 	const buildFirstLevel = () => {
 		return (
 			<>
 				{firstLevelMenu.map((m) => (
 					<div key={m.route}>
-						<a href={`/${m.route}`}>
-							<div
-								className={cn(styles.firstLevel, {
-									[styles.firstLevelActive]: m.id == firstCategory,
-								})}
-							>
-								{m.icon}
-								<span>{m.name}</span>
-							</div>
-						</a>
+						<Link href={`/${m.route}`} legacyBehavior>
+							<a>
+								<div
+									className={cn(styles.firstLevel, {
+										[styles.firstLevelActive]: m.id == firstCategory,
+									})}
+								>
+									{m.icon}
+									<span>{m.name}</span>
+								</div>
+							</a>
+						</Link>
+
 						{m.id == firstCategory && buildSecondLevel(m)}
 					</div>
 				))}
@@ -43,18 +52,28 @@ export const Menu = (): JSX.Element => {
 	const buildSecondLevel = (menuItem: FirstLevelMenuItem) => {
 		return (
 			<div className={styles.secondBlock}>
-				{menu.map((m) => (
-					<div key={m._id.secondCategory}>
-						<div className={styles.secondLevel}>{m._id.secondCategory}</div>
-						<div
-							className={cn(styles.secondLevelBlock, {
-								[styles.secondLevelBlockOpened]: m.isOpened,
-							})}
-						>
-							{buildThirdLevel(m.pages, menuItem.route)}
+				{menu.map((m) => {
+					if (m.pages.map((p) => p.alias).includes(router.asPath.split('/')[2])) {
+						m.isOpened = true;
+					}
+					return (
+						<div key={m._id.secondCategory}>
+							<div
+								className={styles.secondLevel}
+								onClick={() => openSecondLevel(m._id.secondCategory)}
+							>
+								{m._id.secondCategory}
+							</div>
+							<div
+								className={cn(styles.secondLevelBlock, {
+									[styles.secondLevelBlockOpened]: m.isOpened,
+								})}
+							>
+								{buildThirdLevel(m.pages, menuItem.route)}
+							</div>
 						</div>
-					</div>
-				))}
+					);
+				})}
 			</div>
 		);
 	};
@@ -62,14 +81,15 @@ export const Menu = (): JSX.Element => {
 	const buildThirdLevel = (pages: PageItem[], route: string) => {
 		return pages.map((p) => (
 			// eslint-disable-next-line react/jsx-key
-			<a
-				href={`/${route}/${p.alias}`}
-				className={cn(styles.thirdLevel, {
-					[styles.thirdLevelActive]: false,
-				})}
-			>
-				{p.category}
-			</a>
+			<Link href={`/${route}/${p.alias}`} legacyBehavior>
+				<a
+					className={cn(styles.thirdLevel, {
+						[styles.thirdLevelActive]: `/${route}/${p.alias}` == router.asPath,
+					})}
+				>
+					{p.category}
+				</a>
+			</Link>
 		));
 	};
 
