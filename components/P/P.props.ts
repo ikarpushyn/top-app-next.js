cd5d23fe66fd10aefb68a7b836b921f521c5d847
Ilija <68541501+ikarpushyn@users.noreply.github.com>
2023-02-01 21:17:51 +0200

    add sort & advantages component

--- components/P/P.props.ts
+++ components/P/P.props.ts
@@ -2,6 +2,6 @@ import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react';
 
 export interface PProps
 	extends DetailedHTMLProps<HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement> {
-	size: 's' | 'm' | 'l';
+	size?: 's' | 'm' | 'l';
 	children: ReactNode;
 }
--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,10 +1,13 @@
-import { Htag } from '../../components';
+import { Advantages, Htag } from '../../components';
 import { TopPageComponentProps } from './TopPageComponent.props';
 import { Tag } from './../../components/Tag/Tag';
 import styles from './TopPageComponent.module.css';
 import { Card } from './../../components/Card/Card';
 import { HhData } from './../../components/HhData/HhData';
 import { TopLevelCategory } from '../../interfaces/page.interface';
+import { Sort } from './../../components/Sort/Sort';
+import { Rating } from './../../components/Rating/Rating';
+import { SortEnum } from '../../components/Sort/Sort.props';
 
 export const TopPageComponent = ({
 	page,
@@ -20,7 +23,12 @@ export const TopPageComponent = ({
 						{products.length}
 					</Tag>
 				)}
-				<span>sortirovka</span>
+				<Sort
+					sort={SortEnum.Rating}
+					setSort={function (sort: SortEnum): void {
+						throw new Error('Function not implemented.');
+					}}
+				/>
 			</div>
 
 			<div>{products && products.map((p) => <div key={p._id}>{p.title}</div>)}</div>
@@ -34,7 +42,22 @@ export const TopPageComponent = ({
 				)}
 			</div>
 
-			{firstCategory == TopLevelCategory.Courses && <HhData {...page.hh} />}
+			{firstCategory == TopLevelCategory.Courses && page.hh && <HhData {...page.hh} />}
+			{page.advantages && page.advantages.length > 0 && (
+				<>
+					<Htag tag={'h2'}> Преимущества </Htag>
+					<Advantages advantages={page.advantages} />
+				</>
+			)}
+			{page.seoText && (
+				<div className={styles.seo} dangerouslySetInnerHTML={{ __html: page.seoText }} />
+			)}
+			<Htag tag={'h2'}> Получаемые навыки </Htag>
+			{page.tags.map((t) => (
+				<Tag key={t} color="primary">
+					{t}
+				</Tag>
+			))}
 		</div>
 	);
 };
