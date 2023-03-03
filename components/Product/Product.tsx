7509794e82cf691403781cddaac0e86ba348991e
Ilija <68541501+ikarpushyn@users.noreply.github.com>
2023-03-03 17:39:46 +0200

    adaptive page

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -7,12 +7,20 @@ import { Tag } from './../Tag/Tag';
 import { Button } from '../Button/Button';
 import { priceRu } from '../../helpers/helpers';
 import { Divider } from '../Divider/Divider';
+import { declOfNum } from './../../helpers/helpers';
+
+import Image from 'next/image';
 
 export const Product = ({ product, className, ...props }: ProductProps): JSX.Element => {
 	return (
 		<Card className={styles.product}>
 			<div className={styles.logo}>
-				<img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} />
+				<Image
+					src={process.env.NEXT_PUBLIC_DOMAIN + product.image}
+					alt={product.title}
+					width={70}
+					height={70}
+				/>
 			</div>
 			<div className={styles.title}>{product.title}</div>
 			<div className={styles.price}>
@@ -38,11 +46,22 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
 			</div>
 			<div className={styles.priceTitle}>цена</div>
 			<div className={styles.creditTitle}>кредит</div>
-			<div className={styles.rateTitle}>{product.reviewCount} отзывов</div>
+			<div className={styles.rateTitle}>
+				{product.reviewCount} {declOfNum(product.reviewCount, ['отзыв', 'отзыва', 'отзывов'])}
+			</div>
 			<Divider className={styles.hr} />
 
 			<div className={styles.description}> </div>
-			<div className={styles.feature}> fichi</div>
+			<div className={styles.feature}>
+				{' '}
+				{product.characteristics.map((c) => (
+					<div className={styles.characteristics} key={c.name}>
+						<span className={styles.characteristicsName}>{c.name}</span>
+						<span className={styles.characteristicsDots}></span>
+						<span className={styles.characteristicsValue}>{c.value}</span>
+					</div>
+				))}
+			</div>
 			<div className={styles.advBlock}>
 				{product.advantages && (
 					<div className={styles.advantages}>
@@ -58,7 +77,7 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
 					</div>
 				)}
 			</div>
-			<Divider className={styles.hr} />
+			<Divider className={cn(styles.hr, styles.hr2)} />
 			<div className={styles.actions}>
 				<Button appearance="primary">Узнать подробнее</Button>
 				<Button className={styles.reviewButton} appearance="ghost" arrow="right">
