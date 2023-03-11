312f5d6794ed24dbee631eccce7ae38d63f39372
Ilija <68541501+ikarpushyn@users.noreply.github.com>
2023-03-11 15:15:01 +0200

    Husky

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,26 @@ import { AppProps } from 'next/app';
 import '../styles/globals.css';
 import Head from 'next/head';
 import React from 'react';
+import ym, { YMInitializer } from 'react-yandex-metrika';
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
+	router.events.on('routeChangeComplete', (url: string) => {
+		if (typeof window !== 'undefined') {
+			ym('hit', url);
+		}
+	});
+
 	return (
 		<>
 			<Head>
 				<title>My top - best top </title>
 				<link rel="icon" href="/favicon.ico" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" />
-				
-
+				<link rel="preconnect" href="https://mc.yandex.ru" />
+				<meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
 				<meta property="og:locale" content="ru_RU" />
 			</Head>
+			<YMInitializer accounts={[]} options={{ webvisor: true, defer: true }} version="2" />
 			<Component {...pageProps} />
 		</>
 	);
